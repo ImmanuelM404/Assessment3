6f09cf0b9a150601dc615c736d4e90efdf494acd
ImmanuelM404 <91708047+ImmanuelM404@users.noreply.github.com>
2021-10-31 14:58:20 -0700

    initialCommit

---
+++ profile.js
@@ -0,0 +1,21 @@
+// Profile.js
+// GO Back to part 3 and create profile format better 
+
+let colorBtn = document.querySelector('#color')
+colorBtn.addEventListener('click', () => {
+    //colorBtn.textContent = 'Blue'  // .textContent changes button into assigned value once button is clicked
+    alert('Blue')
+    
+})
+
+let placeBtn = document.querySelector('#place')
+placeBtn.addEventListener('click', () => {
+    //placeBtn.textContent = "Good Question"
+    alert('Good Question')
+})
+
+let ritualBtn = document.querySelector('#ritual')
+ritualBtn.addEventListener('click', () =>{
+    //ritualBtn.textContent = 'Be Water'
+    alert('Be Water')
+})
